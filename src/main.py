ae6702f15ed97095849427367ccefef7f08f2363
Kenneth Kiiza <keniz01@Kenneths-MacBook-Pro.local>
2024-03-12 07:51:35 +0000

    Add pandas to keep data in memory

--- src/main.py
+++ src/main.py
@@ -2,9 +2,33 @@ from pathlib import Path
 import sys
 from tinytag import TinyTag 
 from typing import List
+import pandas as pd
+from utilities import DateTimeUtilities as utils
 
 def run():
-    pass
+    # Get file path
+    folder_path = sys.argv[1]
+    print(f"\nScanning ${folder_path} for music files ...... \n")
+
+    path = Path(folder_path)
+
+    files = path.rglob("**/*.mp3", case_sensitive=False)
+
+    audio_tags = [TinyTag.get(file) for file in files]
+    albums = [{
+        "Artist": tag.artist, 
+        "Album": tag.album,
+        "Track": tag.title, 
+        "TrackLength": utils.format_duration(tag.duration),
+        "Genre": tag.genre,
+        "TrackPosition": tag.track,
+        "TrackTotal": tag.track_total,
+        "Year": tag.year        
+    } for tag in audio_tags]
+
+    df = pd.DataFrame(albums).sort_values(by="Album")  
+    print(df)
+    print(len(df))
 
 if __name__ == "__main__":
     run()
