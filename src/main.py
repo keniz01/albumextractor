6cdd0f351e85db59ffb88914a356412ee40eced3
Kenneth KIIZA <kkiiza@SideTrade.com>
2024-03-15 10:55:01 +0000

    Add logic to only add non-duplicate data.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import sys
 from psycopg2 import OperationalError
 from tinytag import TinyTag 
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import UniqueConstraint, create_engine, Column, Integer, String
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import declarative_base
 import pandas as pd
 from utilities import DateTimeUtilities as utils
 from dotenv import load_dotenv
@@ -39,6 +40,7 @@ class Song(Base):
     track_position = Column(Integer, nullable=True)
     track_total = Column(Integer, nullable=True)
     track_year = Column(Integer, nullable=True)
+    __table_args__ = (UniqueConstraint('artist_name', 'album_title', 'track_title'),)
  
 Base.metadata.create_all(engine)
 
@@ -53,18 +55,24 @@ def run():
 
     audio_tags = [TinyTag.get(file) for file in files]
     albums = [{
-        "artist_name": tag.artist, 
-        "album_title": tag.album,
-        "track_title": tag.title, 
-        "track_length": utils.format_duration(tag.duration),
-        "genre_name": tag.genre,
-        "track_position": tag.track,
-        "track_total": tag.track_total,
-        "track_year": tag.year        
+        "artist_name": tag.artist if tag.artist is not None else "",
+        "album_title": tag.album if tag.album is not None else "",
+        "track_title": tag.title if tag.title is not None else "", 
+        "track_length": utils.format_duration(tag.duration) if tag.duration is not None else 0.0,
+        "genre_name": tag.genre if tag.genre is not None else "",
+        "track_position": tag.track if tag.track is not None else 0,
+        "track_total": tag.track_total if tag.track_total is not None else 0,
+        "track_year": tag.year if tag.year is not None else 0,        
     } for tag in audio_tags]
 
+    def insert_on_conflict_nothing(table, conn, keys, data_iter):
+        data = [dict(zip(keys, row)) for row in data_iter]
+        stmt = insert(table.table).values(data).on_conflict_do_nothing(index_elements=['artist_name', 'album_title', 'track_title'])
+        result = conn.execute(stmt)
+        return result.rowcount
+
     df = pd.DataFrame(albums)
-    df.to_sql('tbl_song', engine, if_exists='append', index=False)
+    df.to_sql('tbl_song', engine, if_exists='append', index=False, method=insert_on_conflict_nothing)
     result_df = pd.read_sql_query('SELECT * FROM tbl_song', engine)
     print(result_df)
     
